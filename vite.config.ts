e2ba1f0da3da042f0325695cfb4ada941d06ede1
Desislav Damyanov <desislav.s.damyanov@gmail.com>
2022-12-09 04:17:02 +0200

    config path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${resolve(__dirname, 'src/lib')}/`,
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
